424e4ec523d999ac58ad24c9600483704d212e89
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-01 16:43:06 +0100

    create a board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,13 @@
+board = {11:' ', 12:' ', 13:' ',
+         21:' ', 22:' ', 23:' ',
+         31:' ', 32:' ', 33:' '}
+
+def board_display():
+     print(f"|{board[11]}|{board[12]}|{board[13]}|\n"
+           f"|{board[21]}|{board[22]}|{board[23]}|\n"
+           f"|{board[31]}|{board[32]}|{board[33]}|\n")
+
+board_display()
 while True:
     game_mode = input("Press 'p' if you want to play Player vs Player. \n"
                         "Press 'c' if you want to play vs computer.\n"
@@ -11,3 +21,5 @@ while True:
     elif game_mode.lower() == 'q':
         print("Goodbye.")
         break
+# def pvp():
+#
