fee57144e390f03993aa2cd70bf8246204cd46af
Przemysław Kucharczyk <przemek.franciszek.kucharczyk@gmail.com>
2023-01-01 15:54:38 +0100

    added while loop for play and if statement for choiced game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,13 @@
-print("Initialize project")
+while True:
+    game_mode = input("Press 'p' if you want to play Player vs Player. \n"
+                        "Press 'c' if you want to play vs computer.\n"
+                        "For exit press 'q':\n")
+    if game_mode.lower() == 'p':
+        print("you are in p")
+        break
+    elif game_mode.lower() == 'c':
+        print("you are in c")
+        break
+    elif game_mode.lower() == 'q':
+        print("Goodbye.")
+        break
